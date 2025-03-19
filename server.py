a6ad3f24161a2d8e6402d3c3ec6049085f5b47e8
Sudharsan R <sudharsanr.22cse@kongu.edu>
2025-03-18 21:49:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Flask!,updated version"})
+    return jsonify({"message": "Hello, Flask!,updated v1"})
 
 #hai
 
